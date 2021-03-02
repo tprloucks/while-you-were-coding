eada9fd3ea2501c702e8e59a1998a973bd5f242e
BrianCarela <briancarela@gmail.com>
2021-03-02 16:07:47 -0500

    updated

--- main.js
+++ main.js
@@ -6,64 +6,75 @@
 //
 
 
-
 // ##### Challenges #####
 
-
+console.log('##### Challenge 1 #####');
 // 1.
 //
 // Console out the numbers from 1-20, but only if they're divisible by 3.
-
-// Use `if` and a modulus operation.
-
-
-// 3.
-//
-// Same problem as #2, but no `if`s, `mods`, or `but`s (and no, `but` is not a JavaScript tool). There may be more ways to solve this problem without `if` or `%` (there usually are in JavaScript!), but if you did it the way we did, you should run the loop 6 times with console log per loop iteration.
+// Hint: Use `if` and a modulus operation.
 
 
 
-// 4.
+console.log('##### Challenge 2 #####');
+// 2.
 //
 // Console out the numbers from 1-20. BACKWARDS.
 
 
-// 5.
+
+console.log('##### Challenge 3 #####');
+// 3.
 //
 // For the string 'Boy howdy am I good at this!', console out the letters
 // individually.
+let string3 = 'Boy howdy am I good at this!';
 
 
-// 6.
+
+console.log('##### Challenge 4 #####');
+// 4.
 //
 // For the string, 'And getting better every day.', starting with the second
 // character, print out every third character.
+let string4 = 'And getting better every day.';
 
 
-// 7. 
+
+console.log('##### Challenge 5 #####');
+// 5. 
 //
 // For the string 'Am I the best?', console out the letters individually, AND
 // UPPERCASED.
+let string5 = 'Am I the best?';
 
 
-// 8.
+
+console.log('##### Challenge 6 #####');
+// 6.
 //
 // For the string 'Whoa, I am the best!', console out the letters individually,
 // but also capitalize the letters in the third word.
+let string6 = 'Whoa, I am the best!';
 
 
-// 9.
+
+console.log('##### Challenge 7 #####');
+// 7.
 //
 // For the string 'I am become death, destroyer of worlds.', console out the letters
 //  individually, but backwards. That is, the first console log should be
 // `.`, followed by `s`, followed by `d`, and so on.
+let string7 = 'I am become death, destroyer of worlds.';
 
 
-// 10.
+
+console.log('##### Challenge 8 #####');
+// 8.
 //
-const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|Bogdan|Sharod|David|LaToddra|Carlos';
+const names = 'Alex|Mesuara|Brian|Chanel|Matt|Jeremy B.|Jose|Andy|Dustin|Joshua|Steve H.|Cesar|Nastya|Jeremy L.|Michael|John|Lee|Ruth|Steve M.|Tim|Ronny|Stevie R|Neo|Rick|Kyle|Minahil|Adam|Sonny|James|Daisy|Willie';
 
-// Console out the individual letters of each person in class today, which are
+// Console out the individual letters of each person in class, which are
 // in the string above. Every time you start a new name (including the first
 // one!), console out the string: 'Here today is:'.
 
@@ -72,6 +83,5 @@ const names = 'Colin|Mesuara|Denis|Wilson|Michael|Patrick|DeAundre|Jumary|Marc|B
 // Don't print the pipes!
 
 
-// 11.
-//
-// There is no 11. Celebrate! You did it!
+
+
